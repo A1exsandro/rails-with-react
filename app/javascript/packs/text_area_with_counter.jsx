503a9f1958fbb96c7a93f5b5c942a3aa551e16a9
A1exsandro <alexsandro.acp@gmail.com>
2022-07-28 09:35:06 -0400

    use react component

--- app/javascript/packs/text_area_with_counter.jsx
+++ app/javascript/packs/text_area_with_counter.jsx
@@ -2,23 +2,40 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class TextAreaWithCounter extends Component {
+  state = {
+    content: '',
+    statusColor: ''
+  }
+
+  handleContentChange = (e) => {
+    this.setState({content: e.target.value.slice(0,this.props.limit)});
+  }
   render() {
     return (
-      <textarea 
-        name="posts[content]" 
-        cols="80" 
-        rows="10"
-      >
-        
-      </textarea>
+      <>
+        <div>
+          <textarea 
+            name="posts[content]" 
+            cols="80" 
+            rows="10"
+            value={this.state.content}
+            onChange={this.handleContentChange}
+          >  
+          </textarea>
+        </div>
+
+        <small>Total de caracteres: {this.state.content.length}/{this.props.limit}</small>
+      </>
     );
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector('.text-area-with-counter');
+
   ReactDOM.render(
-    <TextAreaWithCounter />,
-    document.querySelector('.text-area-with-counter')
+    <TextAreaWithCounter limit={element.dataset.limit} />,
+    element  
   )
 });
 
